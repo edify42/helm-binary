57a0448f4c4dc4c909c55396ca687ff2bae3d000
ed kim <ekim8015@gmail.com>
2021-07-21 10:36:34 +1000

    empty bin file...

--- lib/postinstall.sh
+++ lib/postinstall.sh
@@ -4,10 +4,10 @@ binary=$(echo $npm_execpath)
 
 if [[ $binary == *"npm"* ]]; then
   echo "npm It's there!"
-  rm bin/helm && echo 'creating a hard link for helm' && ln node_modules/helm-binary-linux/bin/helm bin/helm || ln node_modules/helm-binary-darwin/bin/helm bin/helm
+  rm -f bin/helm && echo 'creating a hard link for helm' && ln node_modules/helm-binary-linux/bin/helm bin/helm || ln node_modules/helm-binary-darwin/bin/helm bin/helm
 fi
 
 if [[ $binary == *"yarn"* ]]; then
   echo "yarn It's there!"
-  rm bin/helm && echo 'creating a hard link for helm' && ln ../helm-binary-linux/bin/helm bin/helm || ln ../helm-binary-darwin/bin/helm bin/helm
+  rm -f bin/helm && echo 'creating a hard link for helm' && ln ../helm-binary-linux/bin/helm bin/helm || ln ../helm-binary-darwin/bin/helm bin/helm
 fi
